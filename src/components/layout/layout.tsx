31cd5dea332a70050e249377b0d2b44ca28e3141
FranLobo <floboflores@gmail.com>
2022-07-11 15:16:04 +0200

    Add layout and list in componenets

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from 'react';
+import { iRouterItem } from '../../interfaces/interfaces';
+import { Footer } from './footer';
+import { Header } from './header';
+
+export function Layout({
+  children,
+  navOptions,
+}: {
+  children: ReactNode;
+  navOptions: iRouterItem[];
+}) {
+  return (
+    <>
+      <div className="phone">
+        <Header navOptions={navOptions} />
+        <section className="title">
+          <h1>BLUFFING</h1>
+          <p>Explore millions of games according to your taste</p>
+        </section>
+        <section className="search">
+          <div className="search-inner">
+            <button className="search-button">
+              <img src="./images/lupa.png" className="lupa" alt="" />
+            </button>
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search your Game"
+            />
+          </div>
+        </section>
+
+        <nav className="navigation">
+          <a href="./home.html" className="navigation-item active">
+            Home
+          </a>
+          <a href="./pages/ranking.html" className="navigation-item">
+            Ranking
+          </a>
+          <a href="./pages/ranking.html" className="navigation-item">
+            Favourite
+          </a>
+          <a href="./pages/themes.html" className="navigation-item">
+            Themes
+          </a>
+        </nav>
+        <section className="playlists"></section>
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+}
--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -5,7 +5,7 @@ export function List({ data }: { data: iGame[] }) {
   return (
     <ul>
       {data.map((item) => (
-        <li key={item._id}>
+        <li key={item._id as string}>
           <Card game={item} />
         </li>
       ))}
